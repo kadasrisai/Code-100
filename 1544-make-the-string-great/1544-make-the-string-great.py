a5204826cf0eabb79788f170cb5711b908a55743
Srisai Kada <81977942+kadasrisai@users.noreply.github.com>
2024-04-05 15:39:48 +0530

    Time: 19 ms (39.81%), Space: 11.8 MB (29.38%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def makeGood(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack=[]
+        for char in s:
+            if stack and abs(ord(char)-ord(stack[-1]))==32:
+                stack.pop()
+            else:
+                stack.append(char)
+        return ''.join(stack)
+            
