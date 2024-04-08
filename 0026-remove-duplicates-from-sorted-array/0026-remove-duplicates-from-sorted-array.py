7097e8733f62fabf24970c72d422208578779b4d
Srisai Kada <81977942+kadasrisai@users.noreply.github.com>
2024-04-08 14:06:11 +0530

    Time: 51 ms (90.59%), Space: 13.3 MB (28.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: int
         """
         i=0
-        for j in range(1,len(nums)):
+        for j in range(1, len(nums)):
             if nums[j]!=nums[i]:
                 nums[i+1]=nums[j]
                 i+=1
