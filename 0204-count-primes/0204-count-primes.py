1e2542771e0538c26422153134a79cf0aaec4f87
Srisai Kada <81977942+kadasrisai@users.noreply.github.com>
2024-04-10 23:11:24 +0530

    Time: 1780 ms (81.61%), Space: 248.4 MB (13.22%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        seen, ans = [0] * n, 0
+        for num in range(2, n):
+            if seen[num]: continue
+            ans += 1
+            seen[num*num:n:num] = [1] * ((n - 1) // num - num + 1)
+        return ans
+        
