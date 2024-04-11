23ac7d46745050ca1a65f6e29bd6be5d8f69e1aa
Srisai Kada <81977942+kadasrisai@users.noreply.github.com>
2024-04-11 16:13:47 +0530

    Time: 17 ms (81.86%), Space: 11.8 MB (76.33%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(1,len(nums)):
+            nums[i]=nums[i-1]+nums[i]
+            
+        return nums
+            
