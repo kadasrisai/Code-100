d09a3975d76d696fd0eeb85c08577164a0cb5f71
Srisai Kada <81977942+kadasrisai@users.noreply.github.com>
2024-04-18 21:24:26 +0530

    Time: 14 ms (78.12%), Space: 11.5 MB (75.01%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,21 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        n=len(nums)
-        zero_count=[]
-        one_count=[]
-        two_count=[]
-        for i in range(n-1,-1,-1):
-            if nums[i]==0:
-                zero_count.append(0)
-                nums.pop(i)
-            elif nums[i]==1:
-                one_count.append(1)
-                nums.pop(i)
-            elif nums[i]==2:
-                two_count.append(2)
-                nums.pop(i)
-        nums.extend(zero_count)
-        nums.extend(one_count)
-        nums.extend(two_count)
+        nums.sort()
         print(nums)
