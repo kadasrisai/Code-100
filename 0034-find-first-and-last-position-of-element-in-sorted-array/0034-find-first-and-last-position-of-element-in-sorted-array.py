c8c2284724094b0d70f55692a1948e4bad6ff8a7
Srisai Kada <81977942+kadasrisai@users.noreply.github.com>
2024-04-30 19:30:36 +0530

    Time: 58 ms (52.35%), Space: 12.7 MB (89.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -5,26 +5,26 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        start, end = 0, len(nums)
-        left, right = -1, -1
-        
-        while start < end:
-            mid = (start + end) // 2
-            if nums[mid] >= target:
-                end = mid
+        left=self.binarySearch(nums,target,True)
+        right=self.binarySearch(nums,target,False)
+        return [left,right]
+    def binarySearch(self,nums,target,leftbias):
+        l=0
+        r=len(nums)-1
+        i=-1
+        while l<=r:
+            m=(l+r)//2
+            if nums[m]>target:
+                r=m-1
+            elif nums[m]<target:
+                l=m+1
             else:
-                start = mid + 1
-        left = start
-        
-        start, end = 0, len(nums)
-        while start < end:
-            mid = (start + end) // 2
-            if nums[mid] > target:
-                end = mid
-            else:
-                start = mid + 1
-        right = start
-        
-        return [-1, -1] if left == right else [left, right - 1]
+                i=m
+                if leftbias:
+                    r=m-1
+                else:
+                    l=m+1
+        return i
+            
         
         
