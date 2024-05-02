f87a7f65d1f20066a8311ef69870dd864bd06739
Srisai Kada <81977942+kadasrisai@users.noreply.github.com>
2024-05-02 19:27:34 +0530

    Time: 25 ms (94.18%), Space: 12.2 MB (33.69%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -5,8 +5,37 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        for i in nums:
-            if i==target :
+        n = len(nums)  # size of the array
+        low, high = 0, n - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            # if mid points to the target
+            if nums[mid] == target:
                 return True
+
+            # Edge case:
+            if nums[low] == nums[mid] and nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+                continue
+
+            # if left part is sorted
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    # element exists
+                    high = mid - 1
+                else:
+                # element does not exist
+                    low = mid + 1
+            else:  # if right part is sorted
+                if nums[mid] <= target <= nums[high]:
+                    # element exists
+                    low = mid + 1
+                else:
+                    # element does not exist
+                    high = mid - 1
+
         return False
         
