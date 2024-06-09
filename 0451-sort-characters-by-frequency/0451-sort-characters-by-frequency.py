0cdea212013960cfe3177b7df3026a162a4ffb09
Srisai Kada <81977942+kadasrisai@users.noreply.github.com>
2024-06-09 23:36:10 +0530

    Time: 27 ms (78.92%), Space: 14 MB (83.33%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # a string to store the final answer string
+        answerString = ''
+        # a dictionary that stores the frequency of each character
+        hashMap = defaultdict(int)
+        for i in s:
+            hashMap[i] = hashMap[i] + 1
+        # sorting the dictionary based on the values in descending order
+        sortedHashMap = sorted(hashMap.items(), key=lambda x: x[1], reverse=True)
+        # multiply the char with the frequency
+        for char, frequency in sortedHashMap:
+            answerString = answerString + char * frequency
+
+        return answerString
+        
