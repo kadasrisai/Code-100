be2f8ae37a46eb89fc0fd89ae19585cd8c7f15f3
Srisai Kada <81977942+kadasrisai@users.noreply.github.com>
2024-06-29 22:10:32 +0530

    Time: 1042 ms (82.45%), Space: 58 MB (39.07%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -4,28 +4,18 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        # Sort intervals by their end time
-        intervals.sort(key=lambda x: x[1])
+        result = 0
+        intervals = sorted(intervals , key = lambda x : x[1])
+        last_end = intervals[0][1]
         
-        # Initialize variables
-        end = intervals[0][1]
-        count = 1
-        
-        # Iterate over the rest of the intervals
-        for i in range(1, len(intervals)):
-            # If the start time of the current interval is greater than or equal to the end time of the previous interval, they don't overlap
-            if intervals[i][0] >= end:
-                # Update end time to be the end time of the current interval
-                end = intervals[i][1]
-                # Increment the count of non-overlapping intervals
-                count += 1
-            # If they overlap, discard the current interval
-            # (we don't need to update end time or count)
-            # because the previous interval has a smaller end time and a higher priority
+        for interval in intervals[1:]:
+            current_start = interval[0]
+            current_end = interval[1]
+            
+            if current_start >= last_end:
+                last_end = current_end
             else:
-                continue
-        
-        # The number of intervals to remove is the difference between the total number of intervals and the count of non-overlapping intervals
-        return len(intervals) - count
-
+                result = result + 1
+                
+        return result
         
